c82bae6d094811203ac32375c50d5c9a3e9f2e0b
Roman <roma.ablyazoff@yandex.ru>
2022-10-28 12:28:16 +0300

    add point 1,2,3,4,5,6

--- Program.cs
+++ Program.cs
@@ -1,17 +1,122 @@
 ﻿using Aspose.Cells;
 using static System.Console;
 
-Workbook wb = new Workbook("Students.xls"); // открыть существующий xls файл
-int n = Convert.ToInt32(Console.ReadLine());
-//Add new worksheet in existing workbook
-Worksheet sheet = wb.Worksheets.Add($"Groop {n}");
+// проверить или создать xls файл
+Workbook wb = new Workbook("Students.xls");
+int countSheet = wb.Worksheets.Count;
+int cheek = 0;
+int n;
+do
+{
+    WriteLine("------MENU------");
+    WriteLine("1 - Создать группу");
+    WriteLine("2 - Удалить группу");
+    WriteLine("3 - Список групп");
+    WriteLine("4 - Добавить студента/студентов в группу");
+    WriteLine("5 - Состав группы");
+    WriteLine("6 - Удаление студента из группы");
+    WriteLine("0 - exit");
+    
+    n = Convert.ToInt32(ReadLine());
+    switch (n)
+    {
+        case 1:
+            String? GroopName = ReadLine();
+            for (int i = 0; i < countSheet; i++)
+            {
+                if(GroopName == wb.Worksheets[i].Name)
+                {
+                    WriteLine("Такая группа уже существует");
+                    cheek++;
+                    break;
+                }
+            }
+            if (cheek == 0)
+            {
+                Worksheet sheet = wb.Worksheets.Add(GroopName);
+                WriteLine("Группа создана");
+            }
+            wb.Save("Students.xls");
+            break;
+        case 2:
+            GroopName = ReadLine();
+            for (int i = 0; i < countSheet; i++)
+            {
+                if (GroopName == wb.Worksheets[i].Name)
+                {
+                    wb.Worksheets.RemoveAt(GroopName);
+                    WriteLine("Группа удаленна");
+                    cheek++;
+                    break;
+                }
+            }               
+            if (cheek == 0)
+            {
+                WriteLine("нет такой группы");
+            }  
+            wb.Save("Students.xls");         
+            break;
+        case 3:
+            for (int i = 0; i < countSheet; i++)
+            {
+                WriteLine(wb.Worksheets[i].Name);
+            }
+            break;
+        case 4:
+            GroopName = ReadLine();
+            Cell cell;
+            String? NameStudent;
+            for (int j = 0; j < 30; j++)
+            {
+                Write($"{j} - Введите имя студента: ");
+                NameStudent = ReadLine();
+                cell = wb.Worksheets[GroopName].Cells[j,0];
+                cell.PutValue(NameStudent);
+                if (NameStudent?.Length <= 0)
+                {
+                    WriteLine("Запись закончена");
+                    break;
+                }
+            }
+            wb.Save("Students.xls");
+            break;
+        case 5:
+            GroopName = ReadLine();
+            AutoSort(GroopName);
+            for (int j = 0; j < 30; j++)
+            {
+                cell = wb.Worksheets[GroopName].Cells[j,0];
+                WriteLine((j+1)+" - "+cell.StringValue);
+                if(cell.StringValue.Length == 0){
+                    break;
+                }
+            }
+            wb.Save("Students.xls");
+            break;
+        case 6:
+            GroopName = ReadLine();
+            NameStudent = ReadLine();
+            for (int j = 0; j < 30; j++)
+            {
+                cell = wb.Worksheets[GroopName].Cells[j,0];
+                if (NameStudent == cell.StringValue)
+                {
+                    cell.PutValue("");
+                }
+                if(cell.StringValue.Length == 0){
+                    break;
+                }
+            }
+            AutoSort(GroopName);
+            wb.Worksheets[GroopName].Cells.DeleteBlankRows();
+            wb.Save("Students.xls");
+            break;
+        default:
+            break;
+    }
+} while (n != 0);
 
-//Access the "A1" cell in the sheet.
-Cell cell = sheet.Cells["A1"];
-
-//Input the "Hello World!" text into the "A1" cell
-cell.PutValue("Hello World!");
-for (int i = 0; i <= n; i++)
+for (int i = 0; i <= wb.Worksheets.Count; i++)
 {
     if (i == 0)
     {
@@ -19,12 +124,20 @@ for (int i = 0; i <= n; i++)
     }
     wb.Worksheets.RemoveAt($"Evaluation Warning ({i})");
 }
-Aspose.Cells.License license = new Aspose.Cells.License();
-license.SetLicense("Aspose.Cells.lic");
-//Save the Excel file.
 wb.Save("Students.xls");
-/*
-сделать проверку наличия ексель файла, если нету создать
-сделать меню с переходами на создать группу добавить студента и их удаления
-вывод в консоль содержимое группы
-*/
+
+static void AutoSort(string? GroopName){
+    var wb = new Workbook("Students.xls");
+    DataSorter sorter = wb.DataSorter;
+    sorter.Order1 = SortOrder.Ascending;
+    sorter.Key1 = 0;
+    sorter.Order2 =SortOrder.Descending;
+    sorter.Key2 = 1;
+    CellArea ca = new CellArea();
+    ca.StartRow = 0;
+    ca.StartColumn = 0;
+    ca.EndRow = 29;
+    ca.EndColumn = 0;
+    sorter.Sort(wb.Worksheets[GroopName].Cells, ca);
+    wb.Worksheets[GroopName].Cells.DeleteBlankRows();
+}
--- Student.cs
+++
@@ -1,6 +0,0 @@
-﻿class Student
-{
-    public int num {get;set;}
-    public String? name {get;set;}
-    
-}
