b183d1c7fa03ffffabf035560ee22142c4f49ccd
Roman <roma.ablyazoff@yandex.ru>
2022-11-25 09:57:33 +0300

    create new table and new exception

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ String NameFileXLS = "Students.xls";
 Workbook wb = new Workbook(NameFileXLS);
 int countSheet = wb.Worksheets.Count;
 int cheek = 0;
+int idgroop = 0; 
 int n;
 
-do
-{
+Menu:
     WriteLine("------MENU------");
     WriteLine("1 - Создать группу");
     WriteLine("2 - Удалить группу");
-    WriteLine("3 - Список групп");
+    WriteLine("3 - Список групп");;
     WriteLine("4 - Добавить студента/студентов в группу");
     WriteLine("5 - Состав группы / потока");
     WriteLine("6 - Удаление студента из группы");
@@ -22,9 +22,19 @@ do
     WriteLine("9 - Save in exl");
     WriteLine("0 - exit");
 ret:  
-    WriteLine("Введите число: ");
-    n = Convert.ToInt32(ReadLine());
-    if(n > 9){goto ret;}
+    Write("Введите число: ");
+   try
+   {
+        n = Convert.ToInt32(ReadLine());
+        if(n > 9){WriteLine("Ошибка ввода");goto ret;}
+   }
+   catch (Exception)
+   {
+        WriteLine("Неверный формат ввода");
+        goto ret;
+   }
+do
+{
     switch (n)
     {
         case 1:
@@ -36,9 +46,9 @@ ret:
                 WriteLine("Формат записи не верный");
                 goto case 1;
             }
-            try{wb.Worksheets.Add($"{GroopName: ####-##-##}");}catch(Aspose.Cells.CellsException){WriteLine("Группа уже существует");break;}
+            try{wb.Worksheets.Add($"{GroopName: ####-##-##}");}catch(Aspose.Cells.CellsException){WriteLine("Группа уже существует");goto Menu;}
             WriteLine("Группа создана");
-            break;
+            goto Menu;
         case 2:
             Write("Введите полное название группы: ");
             GroopName = ReadLine();
@@ -53,48 +63,66 @@ ret:
                 wb.Worksheets.RemoveAt(GroopName);
                 WriteLine("Группа удаленна");
                 cheek++;
-                break;
+                goto Menu;
             }else{
                 WriteLine("Данной группы нету в списке");
-                break;
+                goto Menu;
             }                     
         case 3:
             try{
-                WriteLine("Список групп / потоков");
+                WriteLine("{0,-2} | {1}","ID", "Группы / Потоки");
                 for (int i = 0; i < countSheet; i++)
-                {
-                    string frame = string.Concat(Enumerable.Repeat("-",wb.Worksheets[i].Name.Length));
-                    WriteLine("/"+ frame +"\\");
-                    WriteLine("|"+wb.Worksheets[i].Name+"|");
-                    WriteLine("\\"+frame+"/");
+                {   
+                    idgroop++;
+                    if(wb.Worksheets[i].Name.StartsWith("Evaluation")) // теперь легче считывать группы / потоки
+                    {
+                        idgroop--;
+                        continue;
+                    }
+                    WriteLine($"{idgroop,-2}"+" | "+wb.Worksheets[i].Name);
                 }
             }catch(System.ArgumentOutOfRangeException){WriteLine("Ошибка: выход индекса за предела");}
             catch(System.TypeInitializationException){WriteLine("Непонятная ошибка");}
-            break;
+            goto Menu;
         case 4:
             Write("Введите название группы: ");
             GroopName = ReadLine();
             GroopName.ToUpper();
+            if(GroopName.Length != 10)
+            {
+                WriteLine("Формат записи не верный");
+                goto case 4;
+            }
             if(!ChekerGroop(GroopName))
             {
                 WriteLine("Формат записи не верный");
                 goto case 4;
             }
+            try{var exist = wb.Worksheets[GroopName].Name;}catch(System.NullReferenceException){WriteLine("Группы / потока не существует"); goto case 4;}
+
             Cell cell;
             String NameStudent;
             for (int j = 0; j < 30; j++)
             {
+                student:
                 Write($"{j+1} - Введите имя студента: ");
                 NameStudent = ReadLine();
+                if (CheckName(NameStudent) == false)
+                {
+                    WriteLine("Имя должны содержать только буквы!");
+                    goto student;
+                }
                 cell = wb.Worksheets[GroopName].Cells[j,0];
                 cell.PutValue(NameStudent);
                 if (NameStudent?.Length <= 0)
                 {
                     WriteLine("Запись закончена");
-                    break;
+                    wb.Worksheets[GroopName].Cells.DeleteBlankRows();
+                    goto Menu;
                 }
             }
-            break;
+            wb.Worksheets[GroopName].Cells.DeleteBlankRows();
+            goto Menu;
         case 5:
             Write("Введите название группы / потока: ");
             GroopName = ReadLine();
@@ -102,32 +130,31 @@ ret:
 
             try{var exist = wb.Worksheets[GroopName].Name;}catch(System.NullReferenceException){WriteLine("Группы / потока не существует"); goto case 5;}
 
+            if (wb.Worksheets[GroopName].Cells.MaxDataRow > 30)
+            {
+                WriteLine("|{0,-3} | {1,-35} | {2,-10}|","ID","ФИО студента","Группа");
+            }
+            else
+            {
+                WriteLine("|{0,-2} | {1,-35}|","ID","ФИО студента");
+            }
+
             for (int j = 0; j <= wb.Worksheets[GroopName].Cells.MaxDataRow; j++)
-            {   if(wb.Worksheets[GroopName].Cells.MaxDataRow > 30){
+            {  
+                if(wb.Worksheets[GroopName].Cells[j,0].StringValue.Length == 0){
+                    goto Menu;
+                } 
+                if(wb.Worksheets[GroopName].Cells.MaxDataRow > 30){
                     var cell1 = wb.Worksheets[GroopName].Cells[j,0];
                     var cell2 = wb.Worksheets[GroopName].Cells[j,1];
-                    string frame1 = string.Concat(Enumerable.Repeat("-",cell1.StringValue.Length));
-                    string frame2 = string.Concat(Enumerable.Repeat("-",cell2.StringValue.Length));
-                    string frame3 = string.Concat(Enumerable.Repeat("-",Convert.ToString(j).Length));
-                    WriteLine($"{frame3}\\/{frame1}\\/{frame2}\\");
-                    WriteLine($"{(j+1)}|{cell1.StringValue}  |{cell2.StringValue,10}|");
-                    WriteLine($"{frame3}/\\{frame1}/\\{frame2}/");
-                    if(cell1.StringValue.Length == 0){
-                        break;
-                    }
+                    WriteLine($"|{(j+1),-3} | {cell1.StringValue,-35} | {cell2.StringValue,-10}|");
                 }else{
-                    
                     cell = wb.Worksheets[GroopName].Cells[j,0];
                     string frame = string.Concat(Enumerable.Repeat("-",cell.StringValue.Length));
-                    WriteLine($"--\\/{frame}\\");
-                    WriteLine((j+1)+"|"+cell.StringValue);
-                    WriteLine($"--/\\{frame}/");
-                    if(cell.StringValue.Length == 0){
-                        break;
-                    }
+                    WriteLine($"|{(j+1),-2}"+" | "+ $"{cell.StringValue,-35}" + "|");
                 }
             }
-            break;
+            goto Menu;
         case 6:
             Write("Введите название группы: ");
             GroopName = ReadLine();
@@ -145,14 +172,14 @@ ret:
                     chekers++;
                 }
                 if(cell.StringValue.Length == 0 || cell.StringValue == null){
-                    break;
+                    goto Menu;
                 }
             }
-            if(chekers == 0){WriteLine($"Студента {NameStudent} нету в списке");break;}
+            if(chekers == 0){WriteLine($"Студента {NameStudent} нету в списке");goto Menu;}
             AutoSortName(GroopName,30);
             wb.Worksheets[GroopName].Cells.DeleteBlankRows();
-           // wb.Save(NameFileXLS);
-            break;
+           
+            goto Menu;
         case 7:
             Write("Введите название потока без года формата ББББ: ");
             String ThreadName = ReadLine();
@@ -170,7 +197,7 @@ ret:
                     goto case 7;
                 }
             }
-            Write("Введите год потока: ");
+            Write("Введите год потока из последних двух цифр: ");
             String EarsThread = ReadLine();
             if(EarsThread.Length != 2)
             {
@@ -208,22 +235,32 @@ ret:
                     addcell = addcell + wb.Worksheets[i].Cells.MaxDataRow+1;
                 }
             }            
-            break;
+            goto Menu;
         case 8:
             Write("Введите название группы / потока: ");
             GroopName = ReadLine();
             GroopName.ToUpper();
-            try{var exist = wb.Worksheets[GroopName].Name;}catch(System.NullReferenceException){WriteLine("Группы не существует"); goto case 6;}
+            try{var exist = wb.Worksheets[GroopName].Name;}catch(System.NullReferenceException){WriteLine("Группы не существует"); goto case 8;}
             int countrow = wb.Worksheets[GroopName].Cells.MaxDataRow;
-            Write("Вы желаете отсортировать поток по Алфваиту ФИО или по группам (1 - по ФИО, 2 - по группам, enter - оставить без изменений): ");
-            int s = Int32.Parse(ReadLine());
-            try{if(s == 1){AutoSortName(GroopName,countrow);}else if(s == 2){AutoSortGrooup(GroopName,countrow);}else{WriteLine("ну лан");}}
-            catch(System.FormatException){WriteLine("ну ни хочешь, как хочешь");}
-            break;
+            Write("Вы желаете отсортировать поток по Алфваиту ФИО или по группам (1 - по ФИО, 2 - по группам, другую любую цифру - оставить без изменений): ");
+            ret1:
+            try{int s = Int32.Parse(ReadLine());
+            if(s == 1){
+                AutoSortName(GroopName,countrow);
+            }else if(s == 2){
+                AutoSortGrooup(GroopName,countrow);
+            }else{
+                WriteLine("ну лан");
+            }}
+            catch(Exception){
+                WriteLine("Ошибка ввода");
+                goto ret1;
+                }
+            goto Menu;
         case 9:
             try{wb.Save(NameFileXLS);}catch(Exception){WriteLine("Error save");break;}
             WriteLine("Сохранение успешно!");
-            break;
+            goto Menu;
         default:
             break;
     }
@@ -245,7 +282,7 @@ bool ChekerGroop(string groopName)
     if(char.IsNumber(groopName[5]) 
         && (char.IsNumber(groopName[6]) && groopName[6] != '0') 
         && char.IsNumber(groopName[8]) 
-        && (char.IsNumber(groopName[9]) && groopName[9] != '0'))
+        && (char.IsNumber(groopName[9])))
     {return true;}
     else{return false;}
 }
@@ -279,4 +316,12 @@ void AutoSortGrooup(string GroopName, int MaxRow){
     sorter.Sort(wb.Worksheets[GroopName].Cells, ca);
     wb.Worksheets[GroopName].Cells.DeleteBlankRows();
 }
-//public static String Repeat(String value,int count) => string.Concat(Enumerable.Repeat(value,count));
+bool CheckName(String StudentName){
+    foreach (char word in StudentName)
+    {
+        if(!char.IsLetter(word)){
+            return false;
+        }
+    }
+    return true;
+}
